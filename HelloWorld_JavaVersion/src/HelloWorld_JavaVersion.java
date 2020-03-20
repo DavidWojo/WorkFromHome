1678acfbfb44c8ff271a9a9cc750cd66e3207005
Wojciechowski, David <davidwojo15@gmail.com>
2020-03-20 11:28:28 -0700

    First commit of first 2 projects

---
+++ HelloWorld_JavaVersion/src/HelloWorld_JavaVersion.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld_JavaVersion
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+		System.out.println("Hello World again.");
+		System.out.println("Line 3");
+	}
+}
