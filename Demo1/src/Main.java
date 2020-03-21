258616d5dd801f46f853006fb66a0e24b5f2c3ed
Wojciechowski, David <davidwojo15@gmail.com>
2020-03-21 12:38:01 -0700

    Added List example, dem iteration with for-each and ListIterator

--- Demo1/src/Main.java
+++ Demo1/src/Main.java
@@ -1,23 +1,59 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.ListIterator;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
 		System.out.println("Starting main()...");
-		testCollections();
+		listExample1();
 		System.out.println("Ending main()....");
 	}
 	
-	private static void testCollections()
+	private static void listExample1()
 	{
-		Collection<String> col = new ArrayList<String>();
-		col.add("David");
-		col.add("Sylvia");
-		for (String s : col)
+		// Create a list of strings
+		List<String> list1 = new ArrayList<>();
+		list1.add("David");
+		list1.add("Sylvia");
+		list1.add("Alice");
+		
+		// iterate over list with for-each construct
+		// Print the string value
+		for (String s : list1)
 		{
 			System.out.println(s);
+			// Note next line compiles fine, but throw exception 
+			// when executing becasue you cannot modify list when
+			// iterating with a for-eacch conbstruct
+			
+			//list1.remove(0);
 		}
+		
+		// Create another list of strings
+		// Iterate over list and remove an element if it is found
+		// You can modify the list if you use an iterator while looping
+		// through the list.  You cannot do this with a for each loop
+		List<String>list2 = new ArrayList<>();
+		list2.add("Kathy");
+		list2.add("Karen");
+		list2.add(0, "Dale");
+		
+		ListIterator<String> listIter = list2.listIterator();
+		while (listIter.hasNext())
+		{
+			String s = listIter.next();
+			if (s.equals("Kathy"))
+			{
+				listIter.remove();
+			}
+			System.out.println("String value is: " + s);
+		}
+		System.out.println("There are " + list2.size() + " names in the list.");
+		
+		
+		
 	}
 }
